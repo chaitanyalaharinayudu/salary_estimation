55b3df101f23cf7f29900447e588e3bb9ed6c377
chaitanyalaharinayudu <81281034+chaitanyalaharinayudu@users.noreply.github.com>
2024-10-15 20:41:35 -0400

    Create datacollection.py

---
+++ datacollection.py
@@ -0,0 +1,8 @@
+import webscraper as gs 
+import pandas as pd 
+
+path = "C:/Users/lahari/Documents/salary_estimation/chromedriver" #path of the file
+
+df = gs.get_jobs('data scientist',1000, False, path, 15)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
