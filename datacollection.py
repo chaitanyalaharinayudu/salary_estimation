831272e190bb8c673ccacac059e574cc5c868ab9
chaitanyalaharinayudu <81281034+chaitanyalaharinayudu@users.noreply.github.com>
2024-10-15 21:24:42 -0400

    Create datacollection.py

--- datacollection.py
+++ datacollection.py
@@ -1,7 +1,7 @@
 import webscraper as gs 
 import pandas as pd 
 
-path = "C:/Users/lahari/Documents/salary_estimation/chromedriver" #path of the file
+path = "C:/Users/Input your file path/Documents/salary_estimation/chromedriver" #path of the file
 
 df = gs.get_jobs('data scientist',1000, False, path, 15)
 
